2c2194ef322e8e8a85235ec9b953303c8d55b7b8
Maximina Shin <maximins@ob-e4.nsk.21-school.ru>
2023-05-17 20:54:45 +0700

    Initial commit

--- src/s21_math.c
+++ src/s21_math.c
@@ -57,6 +57,43 @@ long double s21_cos(double x) {
 }
 
 double s21_sin(double x) { return s21_cos(x - M_PI); }
+#include "s21_math.h"
+
+int s21_abs(int x) {
+  if (x < 0) {
+    x *= -1;
+  }
+  return x;
+}
+
+long double s21_fabs(double x) {
+  if (x < 0.0) {
+    x *= -1.0;
+  }
+  return (long double)x;
+}
+
+long double s21_sqrt(double x) {
+  long double n = x;
+  long double root;
+
+  if (x > 0) {
+    while (1) {
+      root = 0.5 * (n + (x / n));
+      if (s21_fabs(root - n) < 0.0000000000000001) {
+        break;
+      }
+      n = root;
+    }
+  } else if (x == 0) {
+    root = 0;
+  } else {
+    root = my_nan;
+  }
+  return root;
+}
+
+long double s21_pow(double base, double exp) {}
 
 int main() {
   printf("%Lf\n", s21_cos(12.355));
