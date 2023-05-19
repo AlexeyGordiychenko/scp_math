301554178febe21184c833ad04d44c1f00e5c19c
elidacon <elidacon@student.21-school.ru>
2023-05-19 20:09:28 +0500

    Merge with jeanenea

--- src/s21_math.h
+++ src/s21_math.h
@@ -1,8 +1,16 @@
 #define s21_nan (0.0 / 0.0)
 #define s21_PI 3.14159265358979323846
-#define s21_PI_2 1.5707963
+#define s21_nan (0.0 / 0.0)
 #define s21_e (long double)2.718281
-#define s21_epsilon (long double)1e-12
+#define s21_epsilon (long double)0.000001
+
+int compare_float(double f1, double f2);
+
+// Computes cosine
+long double s21_cos(double x);
+
+// Computes sine
+long double s21_sin(double x);
 
 // Computes absolute value of an integer value
 int s21_abs(int x);
@@ -24,21 +32,3 @@ long double s21_ceil(double x);
 
 // Returns the nearest integer not greater than the given value
 long double s21_floor(double x);
-
-// Computes sine
-long double s21_sin(double x);
-
-// Returns the nearest integer not less than the given value
-long double s21_ceil(double x);
-
-// Computes tangent
-long double s21_tan(double x);
-
-// Remainder of the floating-point division operation
-long double s21_fmod(double x, double y);
-
-// Computes arc sine
-long double s21_asin(double x);
-
-// Computes arc cosine
-long double s21_acos(double x);
