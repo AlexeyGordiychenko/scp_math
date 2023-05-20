18cf4c7d6a0a63083f72c832e1129cc7133c13ee
elidacon <elidacon@student.21-school.ru>
2023-05-20 17:42:56 +0500

    Change the output message in case of a fail, so it'll include tests values

--- src/test.c
+++ src/test.c
@@ -1,65 +1,68 @@
 #include "test.h"
 
 START_TEST(abs_tests) {
-  ck_assert_int_eq(s21_abs(tc_int[_i]), abs(tc_int[_i]));
+  int a = s21_abs(tc_int[_i]);
+  int b = abs(tc_int[_i]);
+  ck_assert_msg(a == b, S21_ASSERT_INT_FAIL, tc_int[_i], a, b);
 }
 END_TEST
 
-START_TEST(fabs_tests) { s21_generic_ld_test(_i, s21_fabs, fabs); }
+START_TEST(fabs_tests) { s21_generic_ld_1p_test(_i, s21_fabs, fabs); }
 END_TEST
 
-START_TEST(sqrt_tests) { s21_generic_ld_test(_i, s21_sqrt, sqrt); }
+START_TEST(sqrt_tests) { s21_generic_ld_1p_test(_i, s21_sqrt, sqrt); }
 END_TEST
 
 START_TEST(pow_tests) { s21_generic_ld_2p_test(_i, s21_pow, pow); }
 END_TEST
 
-START_TEST(exp_tests) { s21_generic_ld_test(_i, s21_exp, exp); }
+START_TEST(exp_tests) { s21_generic_ld_1p_test(_i, s21_exp, exp); }
 END_TEST
 
-START_TEST(ceil_tests) { s21_generic_ld_test(_i, s21_ceil, ceil); }
+START_TEST(ceil_tests) { s21_generic_ld_1p_test(_i, s21_ceil, ceil); }
 END_TEST
 
-START_TEST(floor_tests) { s21_generic_ld_test(_i, s21_floor, floor); }
+START_TEST(floor_tests) { s21_generic_ld_1p_test(_i, s21_floor, floor); }
 END_TEST
 
-START_TEST(sin_tests) { s21_generic_ld_test(_i, s21_sin, sin); }
+START_TEST(sin_tests) { s21_generic_ld_1p_test(_i, s21_sin, sin); }
 END_TEST
 
-START_TEST(cos_tests) { s21_generic_ld_test(_i, s21_cos, cos); }
+START_TEST(cos_tests) { s21_generic_ld_1p_test(_i, s21_cos, cos); }
 END_TEST
 
-START_TEST(tan_tests) { s21_generic_ld_test(_i, s21_tan, tan); }
+START_TEST(tan_tests) { s21_generic_ld_1p_test(_i, s21_tan, tan); }
 END_TEST
 
-// START_TEST(fmod_tests) { s21_generic_ld_test(_i, s21_fmod, fmod); }
-// END_TEST
-
-START_TEST(asin_tests) { s21_generic_ld_test(_i, s21_asin, asin); }
+START_TEST(fmod_tests) { s21_generic_ld_2p_test(_i, s21_fmod, fmod); }
 END_TEST
 
-START_TEST(acos_tests) { s21_generic_ld_test(_i, s21_acos, acos); }
+START_TEST(asin_tests) { s21_generic_ld_1p_test(_i, s21_asin, asin); }
+END_TEST
+
+START_TEST(acos_tests) { s21_generic_ld_1p_test(_i, s21_acos, acos); }
 END_TEST
 
 int main() {
   int failed = 0;
   int tc_int_len = sizeof(tc_int) / sizeof(tc_int[0]);
   int tc_double_len = sizeof(tc_double) / sizeof(tc_double[0]);
-  int tc_double_2p_len = sizeof(tc_double_2p) / sizeof(tc_double_2p[0]);
+  int tc_double_2p_len = sizeof(tc_double_2p) / sizeof(tc_double_2p[0]) / 2;
   Suite *s21_math_tests[] = {
       s21_generic_ts(abs_tests, tc_int_len, S21_TITLE_ABS),
       s21_generic_ts(fabs_tests, tc_double_len, S21_TITLE_FABS),
       s21_generic_ts(sqrt_tests, tc_double_len, S21_TITLE_SQRT),
-      s21_generic_ts(pow_tests, tc_double_2p_len / 2, S21_TITLE_POW),
+      s21_generic_ts(pow_tests, tc_double_2p_len, S21_TITLE_POW),
       s21_generic_ts(exp_tests, tc_double_len, S21_TITLE_EXP),
       s21_generic_ts(ceil_tests, tc_double_len, S21_TITLE_CEIL),
       s21_generic_ts(floor_tests, tc_double_len, S21_TITLE_FLOOR),
       s21_generic_ts(sin_tests, tc_double_len, S21_TITLE_SIN),
       s21_generic_ts(cos_tests, tc_double_len, S21_TITLE_COS),
       s21_generic_ts(tan_tests, tc_double_len, S21_TITLE_TAN),
-      // s21_generic_ts(fmod_tests, tc_double_len, S21_TITLE_FMOD),
+      s21_generic_ts(fmod_tests, tc_double_2p_len, S21_TITLE_FMOD),
       s21_generic_ts(asin_tests, tc_double_len, S21_TITLE_ASIN),
-      s21_generic_ts(acos_tests, tc_double_len, S21_TITLE_ACOS), NULL};
+      s21_generic_ts(acos_tests, tc_double_len, S21_TITLE_ACOS),
+      NULL};
 
   for (int i = 0; s21_math_tests[i] != NULL; i++) {
     SRunner *sr = srunner_create(s21_math_tests[i]);
@@ -83,21 +86,23 @@ Suite *s21_generic_ts(const TTest *test_func, int loops_num, char *title) {
   return suite;
 }
 
-void s21_generic_ld_test(int idx, long double (*s21_func)(double),
-                         double (*std_func)(double)) {
+void s21_generic_ld_1p_test(int idx, long double (*s21_func)(double),
+                            double (*std_func)(double)) {
   long double a = s21_func(tc_double[idx]);
   long double b = std_func(tc_double[idx]);
   int a_isnan = isnan(a);
   int a_isinf = isinf(a);
   int b_isnan = isnan(b);
   int b_isinf = isinf(b);
+  int res;
   if (a_isnan || b_isnan) {
-    ck_assert_int_eq(a_isnan, b_isnan);
+    res = a_isnan && b_isnan;
   } else if (a_isinf || b_isinf) {
-    ck_assert_int_eq(a_isinf, b_isinf);
+    res = a_isinf && b_isinf;
   } else {
-    ck_assert_double_eq_tol(a, b, s21_TOLERANCE);
+    res = fabsl(a - b) <= s21_TOLERANCE;
   }
+  ck_assert_msg(res, S21_ASSERT_LD_1P_FAIL, tc_double[idx], a, b);
 }
 
 void s21_generic_ld_2p_test(int idx, long double (*s21_func)(double, double),
@@ -109,12 +114,14 @@ void s21_generic_ld_2p_test(int idx, long double (*s21_func)(double, double),
   int a_isinf = isinf(a);
   int b_isnan = isnan(b);
   int b_isinf = isinf(b);
-
+  int res;
   if (a_isnan || b_isnan) {
-    ck_assert_int_eq(a_isnan, b_isnan);
+    res = a_isnan && b_isnan;
   } else if (a_isinf || b_isinf) {
-    ck_assert_int_eq(a_isinf, b_isinf);
+    res = a_isinf && b_isinf;
   } else {
-    ck_assert_double_eq_tol(a, b, s21_TOLERANCE);
+    res = fabsl(a - b) <= s21_TOLERANCE;
   }
+  ck_assert_msg(res, S21_ASSERT_LD_2P_FAIL, tc_double_2p[idx * 2],
+                tc_double_2p[idx * 2 + 1], a, b);
 }
--- src/test.h
+++ src/test.h
@@ -4,24 +4,38 @@
 
 #include "s21_math.h"
 
-#define S21_TITLE_BG "\033[46m|S21_"
-#define S21_TITLE_END "|\033[0m"
-#define S21_TITLE_ABS S21_TITLE_BG "ABS" S21_TITLE_END
-#define S21_TITLE_FABS S21_TITLE_BG "FABS" S21_TITLE_END
-#define S21_TITLE_SQRT S21_TITLE_BG "SQRT" S21_TITLE_END
-#define S21_TITLE_POW S21_TITLE_BG "POW" S21_TITLE_END
-#define S21_TITLE_EXP S21_TITLE_BG "EXP" S21_TITLE_END
-#define S21_TITLE_CEIL S21_TITLE_BG "CEIL" S21_TITLE_END
-#define S21_TITLE_FLOOR S21_TITLE_BG "FLOOR" S21_TITLE_END
-#define S21_TITLE_SIN S21_TITLE_BG "SIN" S21_TITLE_END
-#define S21_TITLE_COS S21_TITLE_BG "COS" S21_TITLE_END
-#define S21_TITLE_TAN S21_TITLE_BG "TAN" S21_TITLE_END
-#define S21_TITLE_FMOD S21_TITLE_BG "FMOD" S21_TITLE_END
-#define S21_TITLE_ASIN S21_TITLE_BG "ASIN" S21_TITLE_END
-#define S21_TITLE_ACOS S21_TITLE_BG "ACOS" S21_TITLE_END
+#define S21_TITLE "\033[1;46mS21_"
+#define S21_CLR_GRN "\033[32m"
+#define S21_CLR_RED "\033[31m"
+#define S21_CLR_NONE "\033[0m"
+#define S21_TITLE_ABS S21_TITLE "ABS" S21_CLR_NONE
+#define S21_TITLE_FABS S21_TITLE "FABS" S21_CLR_NONE
+#define S21_TITLE_SQRT S21_TITLE "SQRT" S21_CLR_NONE
+#define S21_TITLE_POW S21_TITLE "POW" S21_CLR_NONE
+#define S21_TITLE_EXP S21_TITLE "EXP" S21_CLR_NONE
+#define S21_TITLE_CEIL S21_TITLE "CEIL" S21_CLR_NONE
+#define S21_TITLE_FLOOR S21_TITLE "FLOOR" S21_CLR_NONE
+#define S21_TITLE_SIN S21_TITLE "SIN" S21_CLR_NONE
+#define S21_TITLE_COS S21_TITLE "COS" S21_CLR_NONE
+#define S21_TITLE_TAN S21_TITLE "TAN" S21_CLR_NONE
+#define S21_TITLE_FMOD S21_TITLE "FMOD" S21_CLR_NONE
+#define S21_TITLE_ASIN S21_TITLE "ASIN" S21_CLR_NONE
+#define S21_TITLE_ACOS S21_TITLE "ACOS" S21_CLR_NONE
+#define S21_ASSERT_INT_FAIL                                             \
+  "Failed: value = " S21_CLR_GRN "%d" S21_CLR_NONE                      \
+  " Result: s21 = " S21_CLR_RED "%d" S21_CLR_NONE " std = " S21_CLR_RED \
+  "%d" S21_CLR_NONE
+#define S21_ASSERT_LD_1P_FAIL                                            \
+  "Failed: value = " S21_CLR_GRN "%f" S21_CLR_NONE                       \
+  " Result: s21 = " S21_CLR_RED "%Lf" S21_CLR_NONE " std = " S21_CLR_RED \
+  "%Lf" S21_CLR_NONE
+#define S21_ASSERT_LD_2P_FAIL                                                \
+  "Failed: value1 = " S21_CLR_GRN "%f" S21_CLR_NONE " value2 = " S21_CLR_GRN \
+  "%f" S21_CLR_NONE " Result: s21 = " S21_CLR_RED "%Lf" S21_CLR_NONE         \
+  " std = " S21_CLR_RED "%Lf" S21_CLR_NONE
 
 static const int tc_int[] = {-21, 21, INT_MIN, INT_MAX, 0, -0, +0};
-static const double tc_double[] = {-21.0, 21.0, -41.4567861, 371.67131,
+static const double tc_double[] = {-21.0, 21.0, -41.4567861, 37.67131,
                                    -0.1,  0.1,  0.0,         -0.0,
                                    +0.0,  M_E,  M_PI,        s21_NAN};
 static const double tc_double_2p[] = {
@@ -45,8 +59,8 @@ static const double tc_double_2p[] = {
 
 Suite* s21_generic_ts(const TTest* test_func, int loops_num, char* title);
 
-void s21_generic_ld_test(int idx, long double (*s21_func)(double),
-                         double (*std_func)(double));
+void s21_generic_ld_1p_test(int idx, long double (*s21_func)(double),
+                            double (*std_func)(double));
 
 void s21_generic_ld_2p_test(int idx, long double (*s21_func)(double, double),
                             double (*std_func)(double, double));
