d4e13d23e410b805768a53fb0602cb22fb545a1f
Jeanene Arletta <jeanenea@ob-e2.nsk.21-school.ru>
2023-05-23 18:44:44 +0700

    Added fmod again.

--- src/s21_math.c
+++ src/s21_math.c
@@ -214,11 +214,16 @@ long double s21_cos(double x) {
 long double s21_tan(double x) { return s21_sin(x) / s21_cos(x); }
 
 long double s21_fmod(double x, double y) {
-  long double result;
-  if (y == 0.0) {
+  long double result = 0;
+  if (s21_isnan(x) || s21_isnan(y) || s21_isinf(x) || y == 0) {
     result = s21_NAN;
+  } else if (x == 0 && y != 0 && !s21_isnan(y)) {
+    result = 0;
+  } else if (x != s21_INF && x != -s21_INF && (y == s21_INF || y == -s21_INF)) {
+    result = x;
   } else {
-    result = x - (y * s21_floor(x / y));
+    long long int q = x / y;
+    result = x - q * y;
   }
   return result;
 }
